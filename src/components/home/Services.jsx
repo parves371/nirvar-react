c08859c7f7fe9b0c049b0e787d0e23788eb5eec6
md-parves <parves.dev@gmail.com>
2024-11-04 14:49:20 +0600

    fixed some bug

--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -61,7 +61,7 @@ export const Services = () => {
           </div>
         </div>
       </div>
-
+      
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
           fill="#c1f2f2" // Fill color
